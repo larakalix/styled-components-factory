071c1273268670fb066e3cb4dd9a4896adbf40f0
larakalix <ukikalix@gmail.com>
2022-07-02 03:24:22 -0600

    1 file modified

--- src/styled-factory/styled-factory.js
+++ src/styled-factory/styled-factory.js
@@ -5,6 +5,12 @@ const HEAD = document.head || document.getElementsByTagName('head')[0];
 const STYLE = document.createElement('style');
 HEAD.appendChild(STYLE);
 
+const genRandomName = () =>
+  Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(0, 5);
+
 const defaultTemplateLiteralFunction = (strings, ...values) => {
   return strings.reduce((acc, key, index) => {
     acc += key;
@@ -22,11 +28,7 @@ const defaultTemplateLiteralFunction = (strings, ...values) => {
 };
 
 const appendCss = (styles, type) => {
-  const random = Math.random()
-    .toString(36)
-    .replace(/[^a-z]+/g, '')
-    .substr(0, 5);
-  const cssClassName = `${type}tag-${random}`;
+  const cssClassName = `${type}tag-${genRandomName()}`;
   const css = `.${cssClassName}{${styles}}`;
 
   if (STYLE.styleSheet) STYLE.styleSheet.cssText = css;
